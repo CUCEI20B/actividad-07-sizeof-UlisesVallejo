72ff60f1546cf958adb5132dbed3a2ff658f4f67
Ulises Vallejo <jose.vallejo7479@alumnos.udg.mx>
2020-09-29 21:19:17 -0500

    prueba1

--- main.cpp
+++ main.cpp
@@ -3,9 +3,20 @@
 using namespace std;
 
 int main() {
-    int a;
+    string a;
     cin >> a;
-    cout << sizeof(a);
+    if(a == "int")
+        cout << sizeof(int);
+    else if(a == "char")
+        cout << sizeof(char);
+    else if(a == "short")
+        cout << sizeof(short);
+    else if(a == "float")
+        cout << sizeof(float);
+    else if(a == "double")
+        cout << sizeof(double);
+    else if(a == "long")
+        cout << sizeof(long);
 
     return 0;
 }
